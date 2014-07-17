b11067fc54726c948e6955b02653b667ed44332a
AndyBursh <andy.p.hunt91@gmail.com>
2014-07-17 22:04:13 +0100

    Deletes example application from repository, ahead of creating example application repository

--- example/api/NapExample/resources.php
+++
@@ -1,9 +0,0 @@
-<?php
-use \Nap\Resource\Resource;
-use \Nap\Resource\Parameter\Scheme;
-
-return array(
-    new Resource("TodoLists", "/todo-lists", new Scheme\Id(), array(
-        new Resource("Entries", "/entries", new Scheme\Id())
-    ))
-);
--- example/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-requirejs.config({
-    baseUrl: "./js",
-    paths: {
-        "jquery": "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-        "vendor": "./vendor"
-    }
-});
